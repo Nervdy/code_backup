b87855fa27be4ff882bf5e89df1ceb8b30e6ef93
nervdy <450470823@qq.com>
2018-09-27 17:27:59 +0800

    update

--- LeetCode/plusOne.js
+++ LeetCode/plusOne.js
@@ -4,8 +4,18 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  // return String(Number(digits.join('')) + 1).split('').map(i => Number(i))
-  digits[digits.length - 1] = digits[digits.length - 1] + 1
+
+  (function Carrying (index) {
+
+    if (index == -1) return 0    
+    digits[index] = digits[index] + 1
+    digits[index] = digits[index] == 10 ? Carrying(index - 1) : digits[index]
+    return 0
+
+  })(digits.length - 1)
+
+  if (digits[0] == 0) digits.unshift(1)
+  
   return digits
 }
 
@@ -14,9 +24,12 @@ const assert = require('assert')
 let digits1 = [1, 3, 5, 6]
 let digits2 = [1, 2, 3]
 let digits3 = [4, 3, 2, 1]
-let digits4 = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+let digits4 = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]
 
 assert.deepEqual([1, 3, 5, 7], plusOne(digits1))
 assert.deepEqual([1, 2, 4], plusOne(digits2))
 assert.deepEqual([4, 3, 2, 2], plusOne(digits3))
-assert.deepEqual([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4], plusOne(digits4))
+assert.deepEqual([2, 0, 0, 0], plusOne([1, 9, 9, 9]))
+assert.deepEqual([1, 0], plusOne([9]))
+assert.deepEqual([1, 0, 0], plusOne([9, 9]))
+assert.deepEqual([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4], plusOne(digits4))
