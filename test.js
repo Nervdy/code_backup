235d458eb6bcfbeb73a127d7f4f8f21a7f5048a8
nervdy <450470823@qq.com>
2018-09-28 17:27:22 +0800

    update

--- test.js
+++ test.js
@@ -65,40 +65,56 @@
 
 // console.log('end');
 
-console.log('1');
+// console.log('1');
 
-setTimeout(function() {
-    console.log('2');
-    process.nextTick(function() {
-        console.log('3');
-    })
-    new Promise(function(resolve) {
-        console.log('4');
-        resolve();
-    }).then(function() {
-        console.log('5')
-    })
-})
+// setTimeout(function() {
+//     console.log('2');
+//     process.nextTick(function() {
+//         console.log('3');
+//     })
+//     new Promise(function(resolve) {
+//         console.log('4');
+//         resolve();
+//     }).then(function() {
+//         console.log('5')
+//     })
+// })
 
-new Promise(function(resolve) {
-    console.log('7');
-    resolve();
-}).then(function() {
-    console.log('8')
-})
-process.nextTick(function() {
-  console.log('6');
-})
+// new Promise(function(resolve) {
+//     console.log('7');
+//     resolve();
+// }).then(function() {
+//     console.log('8')
+// })
+// process.nextTick(function() {
+//   console.log('6');
+// })
 
-setTimeout(function() {
-    console.log('9');
-    process.nextTick(function() {
-        console.log('10');
-    })
-    new Promise(function(resolve) {
-        console.log('11');
-        resolve();
-    }).then(function() {
-        console.log('12')
-    })
-})
+// setTimeout(function() {
+//     console.log('9');
+//     process.nextTick(function() {
+//         console.log('10');
+//     })
+//     new Promise(function(resolve) {
+//         console.log('11');
+//         resolve();
+//     }).then(function() {
+//         console.log('12')
+//     })
+// })
+
+var examplearr = [8, 94, 15, 88, 55, 76, 21, 39, 1];
+function sortarr(arr) {
+  for (i = 0; i < arr.length - 1; i++) {
+    for (j = 0; j < arr.length - 1 - i; j++) {
+
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+
+    }
+  }
+  return arr;
+}
+sortarr(examplearr);
+console.log(examplearr);
