25cbc60f02f91153f0cfcb66a4080c772bd5a665
nervdy <450470823@qq.com>
2018-10-08 17:32:20 +0800

    update

--- test.js
+++ test.js
@@ -19,7 +19,6 @@
 // ).then(() => console.log('finish'))
 
 
-
 // function main(n) {
 //   if (n === 0) {
 //     return 'finish'
@@ -103,6 +102,8 @@
 //     })
 // })
 
+
+
 var examplearr = [8, 94, 15, 88, 55, 76, 21, 39, 1];
 function sortarr(arr) {
   for (i = 0; i < arr.length - 1; i++) {
@@ -118,3 +119,18 @@ function sortarr(arr) {
 }
 sortarr(examplearr);
 console.log(examplearr);
+
+
+// var examplearr = [8, 94, 15, 88, 55, 76, 21, 39, 1];
+
+// function sort (arr) {
+//   for (let i=0; i < arr.length; i++) {
+//     for (let j=arr.length - 1; j > i; j--) {
+//       if (arr[j] < arr[j - 1]) {
+//         [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+//       }
+//     }
+//   }
+//   return arr
+// }
+// console.log(sort(examplearr))
