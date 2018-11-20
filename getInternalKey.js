64ab9a44ed4867fb7054f9dc5a60b37fe0d26481
nervdy <450470823@qq.com>
2018-11-20 11:49:51 +0800

    add getInternalKey

---
+++ getInternalKey.js
@@ -0,0 +1,34 @@
+// 挑战目标：获取 key 的值
+(function () {
+  // 一个内部变量，外部无法获取
+  var key = Math.random()
+
+  console.log('[test] key:', key)
+
+  // 一个内部函数
+  function internal(x) {
+    return x
+  }
+
+  // 对外暴露的函数
+  apiX = function (x) {
+    try {
+      return internal(x)
+    } catch (err) {
+      return key
+    }
+  }
+})()
+
+// 你的代码写在此处：
+// ...
+
+function d() {
+  try {
+    return d()
+  } catch (e) {
+    return apiX()
+  }
+}
+
+console.log(d());
