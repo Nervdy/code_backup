a14b22273cb0e3a9a96c901b4f543ed1bb715fea
Nervdy <450470823@qq.com>
2019-01-23 01:46:50 +0000

    gitpod test commit

--- test.js
+++ test.js
@@ -40,26 +40,26 @@
 //     console.log(333);
 //   })
 //   process.nextTick(() => {  // callback4
-//     console.log(444);  
+//     console.log(444);
 //   })
 // }, 0);
 
 // setImmediate(() => {        // callback5
 //   console.log(555);
 //   process.nextTick(() => {  // callback6
-//     console.log(666);  
+//     console.log(666);
 //   })
 // })
 
-// setTimeout(() => {          // callback7              
+// setTimeout(() => {          // callback7
 //   console.log(777);
 //   process.nextTick(() => {  // callback8
-//     console.log(888);   
+//     console.log(888);
 //   })
 // }, 0);
 
 // process.nextTick(() => {    // callback9
-//   console.log(999);  
+//   console.log(999);
 // })
 
 // console.log('end');
@@ -163,3 +163,5 @@ function doubleAfter1Sec (param) {
     }, 1000)
   })
 }
+
+// test commit
